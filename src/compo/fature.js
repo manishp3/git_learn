3576fc912c078ce3d12dd9cf18ceedfab3cecefc
Manish Patadiya <patadiyamanish20@gmail.com>
2026-03-28 15:10:53 +0530

    manish

---
+++ src/compo/fature.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const fature = () => {
+  return (
+    <div>This is Feature</div>
+  )
+}
+
+export default fature
---
+++ src/compo/second.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const second = () => {
+  return (
+    <div>This is second</div>
+  )
+}
+
+export default second
